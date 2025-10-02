7eeb2abe43362ac244a264e90df754a4cc7ce703
Suraj <surajpassi5@gmail.com>
2025-10-02 13:10:59 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,16 @@
+# all directories
+mkdir -p src
+mkdir -p research
+
+# all files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynnb
+touch requirements.txt
+touch README.md 
+
+echo "ALL file done "
